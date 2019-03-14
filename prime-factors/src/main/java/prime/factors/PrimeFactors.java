8b47893835f59bef21b73da3c1f8abe3b5036940
ashah21 <anya.shah21@gmail.com>
2019-03-14 14:27:03 -0400

    Update the PrimeFactor code
    Now you can let n be any number and the code will figure out the prime factors of that number

--- prime-factors/src/main/java/prime/factors/PrimeFactors.java
+++ prime-factors/src/main/java/prime/factors/PrimeFactors.java
@@ -4,7 +4,30 @@ public class PrimeFactors {
 	
 	public static void main(String[] args) {
 		//Java requires an entry point to start. Put code in here to call generate()
-
+		
+		int n = 5;
+		System.out.print(generate(n) + " ");
+		//System.out.print(generate(5) + ": " );
+		
+	}
+	
+	
+	public static int generate(int n)
+	{
+		int i = 2;
+		 while( n >1)
+		 {
+			 if(n %i ==0)
+			 {
+				 System.out.print(i + " ");
+				 n = n/i;
+			 }
+			 else
+				 i++;
+		 }
+		
+		return n;
+		
 	}
 
 }
